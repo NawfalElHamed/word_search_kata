1ec6a717e9bb1ea348458e02767618641612e159
Nawfal Elhamed <nawfalelhamed99@gmail.com>
2026-03-09 01:25:52 +0000

    🔴 test: red — horizontal word search

---
+++ test/wordSearch.test.js
@@ -0,0 +1,18 @@
+const { findWord } = require("../src/wordSearch")
+describe("Word Search Kata", () => {
+    test("finds KHAN left to right", () => {
+        const grid = [
+            ["K", "H", "A", "N"],
+            ["B", "O", "N", "E"],
+        ];
+
+        const result = findWord(grid, "KHAN")
+
+        expect(result).toEqual([
+            [0, 0],
+            [1, 0],
+            [2, 0],
+            [3, 0]
+        ])
+    })
+})
