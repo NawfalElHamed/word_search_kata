460fbe31a8211913b651bacde21b6ae246de3436
Nawfal Elhamed <nawfalelhamed99@gmail.com>
2026-03-10 11:03:14 +0000

    🔴 test: red —add failing test for formatting search results

--- test/wordSearch.test.js
+++ test/wordSearch.test.js
@@ -1,194 +1,220 @@
-const { findWord, inputToWordAndGrid, findMultipleWords } = require("../src/wordSearch")
+const {
+  findWord,
+  inputToWordAndGrid,
+  findMultipleWords,
+  formatResults,
+} = require("../src/wordSearch");
 
 describe("Word Search Kata", () => {
+  test("finds KHAN left to right", () => {
+    const grid = [
+      ["K", "H", "A", "N"],
+      ["B", "O", "N", "E"],
+      ["T", "E", "S", "T"],
+    ];
 
+    const result = findWord(grid, "KHAN");
 
-    test("finds KHAN left to right", () => {
-        const grid = [
-            ["K", "H", "A", "N"],
-            ["B", "O", "N", "E"],
-            ["T", "E", "S", "T"],
-        ];
+    expect(result).toEqual([
+      [0, 0],
+      [1, 0],
+      [2, 0],
+      [3, 0],
+    ]);
+  });
 
-        const result = findWord(grid, "KHAN")
+  test("finds KHAN top to bottom", () => {
+    const grid = [
+      ["K", "H", "A", "E"],
+      ["H", "H", "A", "E"],
+      ["A", "H", "A", "E"],
+      ["N", "H", "A", "E"],
+    ];
 
-        expect(result).toEqual([
-            [0, 0],
-            [1, 0],
-            [2, 0],
-            [3, 0]
-        ])
-    })
+    const result = findWord(grid, "KHAN");
 
-    test("finds KHAN top to bottom", () => {
-        const grid = [
-            ["K", "H", "A", "E"],
-            ["H", "H", "A", "E"],
-            ["A", "H", "A", "E"],
-            ["N", "H", "A", "E"],
-        ]
+    expect(result).toEqual([
+      [0, 0],
+      [0, 1],
+      [0, 2],
+      [0, 3],
+    ]);
+  });
 
-        const result = findWord(grid, "KHAN")
+  test("finds KHAN diagonally down to right", () => {
+    const grid = [
+      ["K", "X", "X", "X"],
+      ["X", "H", "X", "X"],
+      ["X", "X", "A", "X"],
+      ["X", "X", "X", "N"],
+    ];
 
-        expect(result).toEqual([
-            [0, 0],
-            [0, 1],
-            [0, 2],
-            [0, 3]
-        ])
-    })
+    const result = findWord(grid, "KHAN");
 
-    test("finds KHAN diagonally down to right", () => {
-        const grid = [
-            ["K", "X", "X", "X"],
-            ["X", "H", "X", "X"],
-            ["X", "X", "A", "X"],
-            ["X", "X", "X", "N"],
-        ]
+    expect(result).toEqual([
+      [0, 0],
+      [1, 1],
+      [2, 2],
+      [3, 3],
+    ]);
+  });
+  test("finds KHAN right to left", () => {
+    const grid = [
+      ["N", "A", "H", "K"],
+      ["X", "X", "X", "X"],
+      ["X", "X", "X", "X"],
+      ["X", "X", "X", "X"],
+    ];
 
-        const result = findWord(grid, "KHAN")
+    const result = findWord(grid, "KHAN");
 
-        expect(result).toEqual([
-            [0, 0],
-            [1, 1],
-            [2, 2],
-            [3, 3]
-        ])
+    expect(result).toEqual([
+      [3, 0],
+      [2, 0],
+      [1, 0],
+      [0, 0],
+    ]);
+  });
+  test("finds KHAN bottom to top", () => {
+    const grid = [
+      ["N", "H", "A", "E"],
+      ["A", "H", "A", "E"],
+      ["H", "H", "A", "E"],
+      ["K", "H", "A", "E"],
+    ];
 
-    })
-    test("finds KHAN right to left", () => {
-        const grid = [
-            ["N", "A", "H", "K"],
-            ["X", "X", "X", "X"],
-            ["X", "X", "X", "X"],
-            ["X", "X", "X", "X"],
-        ];
+    const result = findWord(grid, "KHAN");
 
-        const result = findWord(grid, "KHAN")
+    expect(result).toEqual([
+      [0, 3],
+      [0, 2],
+      [0, 1],
+      [0, 0],
+    ]);
+  });
+  // diagonally from bottom - left to top - right
+  test("finds KHAN diagonally up to right", () => {
+    const grid = [
+      ["X", "X", "X", "N"],
+      ["X", "X", "A", "X"],
+      ["X", "H", "X", "X"],
+      ["K", "X", "X", "X"],
+    ];
 
-        expect(result).toEqual([
-            [3, 0],
-            [2, 0],
-            [1, 0],
-            [0, 0]
-        ])
-    })
-    test("finds KHAN bottom to top", () => {
-        const grid = [
-            ["N", "H", "A", "E"],
-            ["A", "H", "A", "E"],
-            ["H", "H", "A", "E"],
-            ["K", "H", "A", "E"],
-        ]
+    const result = findWord(grid, "KHAN");
 
-        const result = findWord(grid, "KHAN")
+    expect(result).toEqual([
+      [0, 3],
+      [1, 2],
+      [2, 1],
+      [3, 0],
+    ]);
+  });
 
-        expect(result).toEqual([
-            [0, 3],
-            [0, 2],
-            [0, 1],
-            [0, 0]
-        ])
-    })
-    // diagonally from bottom - left to top - right
-    test("finds KHAN diagonally up to right", () => {
-        const grid = [
-            ["X", "X", "X", "N"],
-            ["X", "X", "A", "X"],
-            ["X", "H", "X", "X"],
-            ["K", "X", "X", "X"],
-        ];
+  // diagonally from top-right to bottom-left
+  test("finds KHAN diagonally down to left", () => {
+    const grid = [
+      ["X", "X", "X", "K"],
+      ["X", "X", "H", "X"],
+      ["X", "A", "X", "X"],
+      ["N", "X", "X", "X"],
+    ];
 
-        const result = findWord(grid, "KHAN");
+    const result = findWord(grid, "KHAN");
 
-        expect(result).toEqual([
-            [0, 3],
-            [1, 2],
-            [2, 1],
-            [3, 0]
-        ]);
-    });
+    expect(result).toEqual([
+      [3, 0],
+      [2, 1],
+      [1, 2],
+      [0, 3],
+    ]);
+  });
+  // diagonally from bottom_left to rop-right
+  test("finds KHAN diagonally up-left", () => {
+    const grid = [
+      ["N", "X", "X", "X"],
+      ["X", "A", "X", "X"],
+      ["X", "X", "H", "X"],
+      ["X", "X", "X", "K"],
+    ];
 
-    // diagonally from top-right to bottom-left
-    test("finds KHAN diagonally down to left", () => {
-        const grid = [
-            ["X", "X", "X", "K"],
-            ["X", "X", "H", "X"],
-            ["X", "A", "X", "X"],
-            ["N", "X", "X", "X"],
-        ];
+    const result = findWord(grid, "KHAN");
 
-        const result = findWord(grid, "KHAN");
-
-        expect(result).toEqual([
-            [3, 0],
-            [2, 1],
-            [1, 2],
-            [0, 3]
-        ]);
-    });
-    // diagonally from bottom_left to rop-right
-    test("finds KHAN diagonally up-left", () => {
-        const grid = [
-            ["N", "X", "X", "X"],
-            ["X", "A", "X", "X"],
-            ["X", "X", "H", "X"],
-            ["X", "X", "X", "K"],
-        ]
-
-        const result = findWord(grid, "KHAN")
-
-        expect(result).toEqual([
-            [3, 3],
-            [2, 2],
-            [1, 1],
-            [0, 0]
-        ])
-    })
-    test("parses the input into words and grid", () => {
-        const input = `
+    expect(result).toEqual([
+      [3, 3],
+      [2, 2],
+      [1, 1],
+      [0, 0],
+    ]);
+  });
+  test("parses the input into words and grid", () => {
+    const input = `
             BONES,KHAN,KIRK
             B,O,N,E,S
             K,H,A,N,X
-            K,I,R,K,X`
+            K,I,R,K,X`;
 
-        const result = inputToWordAndGrid(input)
+    const result = inputToWordAndGrid(input);
 
-        expect(result).toEqual({
-            words: ["BONES", "KHAN", "KIRK"],
-            grid: [
-                ["B", "O", "N", "E", "S"],
-                ["K", "H", "A", "N", "X"],
-                ["K", "I", "R", "K", "X"]
-            ]
-        })
-    })
+    expect(result).toEqual({
+      words: ["BONES", "KHAN", "KIRK"],
+      grid: [
+        ["B", "O", "N", "E", "S"],
+        ["K", "H", "A", "N", "X"],
+        ["K", "I", "R", "K", "X"],
+      ],
+    });
+  });
 
-    //same thing as findWord just this one support multiple words
-    test("finds multiple words in the grid", () => {
+  //same thing as findWord just this one support multiple words
+  test("finds multiple words in the grid", () => {
+    const grid = [
+      ["K", "H", "A", "N"],
+      ["B", "O", "N", "E"],
+    ];
 
-        const grid = [
-            ["K", "H", "A", "N"],
-            ["B", "O", "N", "E"],
-        ]
+    const words = ["KHAN", "BONE"];
 
-        const words = ["KHAN", "BONE"]
+    const result = findMultipleWords(grid, words);
 
-        const result = findMultipleWords(grid, words)
+    expect(result).toEqual({
+      KHAN: [
+        [0, 0],
+        [1, 0],
+        [2, 0],
+        [3, 0],
+      ],
+      BONE: [
+        [0, 1],
+        [1, 1],
+        [2, 1],
+        [3, 1],
+      ],
+    });
+  });
 
-        expect(result).toEqual({
-            KHAN: [
-                [0, 0],
-                [1, 0],
-                [2, 0],
-                [3, 0]
-            ],
-            BONE: [
-                [0, 1],
-                [1, 1],
-                [2, 1],
-                [3, 1]
-            ]
-        })
-    })
-})
+  test("formats the results for all found words", () => {
+    const results = {
+      KHAN: [
+        [0, 0],
+        [1, 0],
+        [2, 0],
+        [3, 0],
+      ],
+      BONE: [
+        [0, 1],
+        [1, 1],
+        [2, 1],
+        [3, 1],
+      ],
+    };
+
+    const words = ["KHAN", "BONE"];
+
+    const formatted = formatResults(results, words);
+
+    expect(formatted).toBe(
+      "KHAN: (0,0),(1,0),(2,0),(3,0)\nBONE: (0,1),(1,1),(2,1),(3,1)",
+    );
+  });
+});
